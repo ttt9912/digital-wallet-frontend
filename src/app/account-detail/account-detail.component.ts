5fcbc5079a84e3c453f5633b41d3ad1fce6ad054
ttt <tschachtli.th@gmail.com>
2024-02-21 15:26:25 +0100

    add feature to sort transactions by date

--- src/app/account-detail/account-detail.component.ts
+++ src/app/account-detail/account-detail.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AsyncPipe, CurrencyPipe, DatePipe, NgClass, NgForOf, NgIf } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { map, Observable } from 'rxjs';
+import { map, Observable, Subscription } from 'rxjs';
 import { Account, AccountTransaction } from '../../model/model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
@@ -20,21 +20,33 @@ import { environment } from '../../environments/environment';
   templateUrl: './account-detail.component.html',
   styleUrl: './account-detail.component.scss'
 })
-export class AccountDetailComponent {
-  account$: Observable<Account>
-  accountTransactions$: Observable<AccountTransaction[]>
+export class AccountDetailComponent implements OnDestroy {
+  account$: Observable<Account>;
+  accountTransactionsSubscription: Subscription;
+  accountTransactions: AccountTransaction[] = []
+  sortDsc = true;
 
   constructor(private http: HttpClient,
               private activatedRoute: ActivatedRoute) {
     const accountId = this.activatedRoute.snapshot.paramMap.get('accountId');
     this.account$ = this.http.get<Account>(`${environment.apiUrl}/accounts/${accountId}`);
-    this.accountTransactions$ = this.http.get<AccountTransaction[]>(`${environment.apiUrl}/accounts/${accountId}/transactions`).pipe(
+
+    this.accountTransactionsSubscription = this.http.get<AccountTransaction[]>(`${environment.apiUrl}/accounts/${accountId}/transactions`).pipe(
       map(accountTransactions => this.sortByBookingDateTimeDsc(accountTransactions))
-    );
+    ).subscribe(res => this.accountTransactions = this.sortByBookingDateTimeDsc(res));
+  }
+
+  ngOnDestroy(): void {
+    this.accountTransactionsSubscription.unsubscribe();
   }
 
   private sortByBookingDateTimeDsc(transactions: AccountTransaction[]): AccountTransaction[] {
     return transactions.sort((a, b) =>
       new Date(b.bookingDateTime).getTime() - new Date(a.bookingDateTime).getTime());
+  }
+
+  onSort() {
+    this.sortDsc = !this.sortDsc;
+    this.accountTransactions.reverse()
   }
 }
